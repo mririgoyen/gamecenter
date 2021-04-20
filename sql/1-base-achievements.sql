0f8a1073c028063a1425cef2723beaa6e5de720c
Michael Irigoyen <michael@irigoyen.dev>
2021-04-19 20:39:28 -0500

    Update base achievements

--- sql/1-base-achievements.sql
+++ sql/1-base-achievements.sql
@@ -10,13 +10,13 @@ INSERT INTO `achievements` (
   `hidden`
 )
 VALUES
-( 'created_avatar', 'Looking Good!', 'Created your first avatar', 'SOMETHING', 1, 0, 0 ),
-( 'dkong_top3', 'DKONG TOP 3', 'DKONG TOP 3 DESC', 'SOMETHING', 1, 0, 0 ),
-( 'dkong_tournament_winner', 'DKONG TOURNY', 'DKONG TOURNY DESC', 'SOMETHING', 1, 0, 0 ),
-( 'extralife_2020', 'EL2020', 'EL2020 DESC', 'SOMETHING', 1, 0, 0 ),
-( 'extralife_2020_team', 'EL2020 TEAM', 'EL2020 TEAM DESC', 'SOMETHING', 1, 0, 0 ),
-( 'galaga_top3', 'GALAGA TOP 3', 'GALAGA TOP 3 DESC', 'SOMETHING', 1, 0, 0 ),
-( 'galaga_tournament_winner', 'GALAGA TOURNY', 'GALAGE TOURNY DESC', 'SOMETHING', 1, 0, 0 ),
-( 'pacman_top3', 'PACMAN TOP 3', 'PACMAN TOP 3 DESC', 'SOMETHING', 1, 0, 0 ),
-( 'pacman_tournament_winner', 'PACMAN TOURNY', 'PACMAN TOURNY DESC', 'SOMETHING', 1, 0, 0 ),
-( 'viewed_about', 'VIEWED ABOUT', 'VIEWED ABOUT DESC', 'NOTHING', 1, 1, 1 );
+( 'created_avatar', 'You Look Good!', 'Created your first avatar.', NULL, 1, 0, 0 ),
+( 'dkong_top3', 'Not Monkeying Around', 'Placed 1st, 2nd, or 3rd in Donkey Kong.', 'Jumpman T-Shirt Graphic', 1, 0, 0 ),
+( 'dkong_tournament_winner', 'King Kong', 'Finished 1st place in a Donkey Kong tournament.', 'High Score T-Shirt Graphic', 1, 0, 0 ),
+( 'extralife_2020', '#ForTheKids - 2020', 'Played a game during Extra Life Game Day 2020.', 'Extra Life T-Shirt Graphic', 1, 0, 0 ),
+( 'extralife_2020_team', 'Extra Life Champions - 2020', 'Helped your team win the 2020 Extra Life Game Day.', 'Golden Controller T-Shirt Graphic', 1, 0, 1 ),
+( 'galaga_top3', 'Galactic Hero', 'Placed 1st, 2nd, or 3rd in Galaga.', 'Gyaraga T-Shirt Graphic', 1, 0, 0 ),
+( 'galaga_tournament_winner', 'Galactic Champion', 'Finished 1st place in a Galaga tournament.', 'High Score T-Shirt Graphic', 1, 0, 0 ),
+( 'pacman_top3', 'Top Chomper', 'Placed 1st, 2nd, or 3rd in PAC-MAN.', 'PAC-MAN T-Shirt Graphic', 1, 0, 0 ),
+( 'pacman_tournament_winner', 'Chomp Master', 'Finished 1st place in a PAC-MAN tournament.', 'High Score T-Shirt Graphic', 1, 0, 0 ),
+( 'viewed_about', 'Easter Egg Hunter', 'Found the hidden About dialog.', NULL, 1, 1, 0 );
